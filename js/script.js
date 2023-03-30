8540b3e7af5d624de2516c85d678b982cb990c0d
JuniorDEV <s0pr4j0g4@gmail.com>
2023-03-30 11:35:47 -0300

    Efeitos JS adicionados

--- js/script.js
+++ js/script.js
@@ -9,3 +9,20 @@ function menuMobile() {
   hamburguer.addEventListener("click", abrirMenu);
 }
 menuMobile();
+
+const jsScroll = document.querySelectorAll(".js-scroll");
+
+function animaScroll() {
+  jsScroll.forEach((sessao) => {
+    const tela = window.innerHeight * 0.5;
+    const sessaoTop = sessao.getBoundingClientRect().top - tela;
+
+    if (sessaoTop < 0) {
+      sessao.classList.add("scroll-ativo");
+    } else {
+      sessao.classList.remove("scroll-ativo");
+    }
+  });
+}
+
+window.addEventListener("scroll", animaScroll);
