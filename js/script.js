7693b68f3b4497937b4048036ce04fdddd167efa
JuniorDEV <s0pr4j0g4@gmail.com>
2023-04-09 13:46:34 -0300

    iciando carrossel projetos

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
 function menuMobile() {
   const hamburguer = document.querySelector(".hamburguer");
   const html = document.querySelector("html");
+  const itens = document.querySelectorAll(".js-seletor");
 
   function abrirMenu() {
     html.classList.toggle("abrir-menu");
   }
 
+  function fecharMenu() {
+    html.classList.remove("abrir-menu");
+  }
+
   hamburguer.addEventListener("click", abrirMenu);
+  itens[0].addEventListener("click", fecharMenu);
+  itens[1].addEventListener("click", fecharMenu);
+  itens[2].addEventListener("click", fecharMenu);
 }
 menuMobile();
 
@@ -27,3 +35,48 @@ function scrollAnimado() {
   window.addEventListener("scroll", animaScroll);
 }
 scrollAnimado();
+
+function scrollSuave() {
+  const linkInterno = document.querySelectorAll("a[href^='#']");
+
+  function scrolSessao(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const sessao = document.querySelector(href);
+    const sessaoTopo = sessao.offsetTop - 80;
+
+    window.scrollTo({
+      top: sessaoTopo,
+      behavior: "smooth",
+    });
+  }
+
+  linkInterno.forEach((link) => {
+    link.addEventListener("click", scrolSessao);
+  });
+}
+scrollSuave();
+
+function tagConhecimentos() {
+  const tecnologia = document.querySelectorAll(".techs img");
+  const tag = document.querySelectorAll(".tech-nome");
+  function abrirTag() {
+    console.log("link");
+  }
+
+  tecnologia.forEach((img, index) => {
+    function abrirTag() {
+      tag[index].classList.add("ativo");
+    }
+
+    function fecharTag() {
+      tag[index].classList.remove("ativo");
+    }
+
+    img.addEventListener("mouseover", abrirTag);
+    img.addEventListener("touchstart", abrirTag);
+    img.addEventListener("mouseout", fecharTag);
+  });
+}
+
+tagConhecimentos();
